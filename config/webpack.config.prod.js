f0ec8622b95661f4339d83f2eda8edac27d8fb2e
Zoffy Zhang <zoffyzhang@foxmail.com>
2018-08-27 15:27:10 +0800

    add stylus-loader

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -167,7 +167,7 @@ module.exports = {
           // use the "style" loader inside the async code so CSS from them won't be
           // in the main CSS file.
           {
-            test: /\.css$/,
+            test: /\.(css)|(styl)$/,
             loader: ExtractTextPlugin.extract(
               Object.assign(
                 {
@@ -204,7 +204,14 @@ module.exports = {
                             flexbox: 'no-2009',
                           }),
                         ],
+                        sourceMap: false
                       },
+                    },
+                    {
+                        loader: require.resolve('stylus-loader'),
+                        options: {
+                            strictMath: true,
+                        },
                     },
                   ],
                 },
